49a1f7b1a1d6f589e3d516777d2eb2883b5f55d2
MY <fourp@naver.com>
2024-03-01 21:46:25 +0900

    스타일 적용하기2

---
+++ 011_Style/my-app/src/Components/Test.jsx
@@ -0,0 +1,11 @@
+import style from './test.module.css'
+import React, { Fragment } from 'react';
+
+export default function test() {
+  return (
+    <>
+        <h1>Test.jsx Test</h1>
+        <h2 className={style.div1}>Test.jsx Test : div2</h2>
+    </>
+  )
+}
