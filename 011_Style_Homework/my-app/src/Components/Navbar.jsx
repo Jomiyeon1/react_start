3a44e5235bfaef35ebd7c9e5cf7f2073ab16ff3a
Mi Yeon <fourp@naver.com>
2024-03-07 16:31:37 +0900

    Quiz navbar 중간 커밋

--- 011_Style_Homework/my-app/src/Components/Navbar.jsx
+++ 011_Style_Homework/my-app/src/Components/Navbar.jsx
@@ -1,15 +1,17 @@
 import style from './navbar.module.css'
-import logo from './logo.svg';
+import logo from '../../src/logo.svg';
 import React, { Fragment } from 'react';
 
 export default function navbar() {
   return (
     <>
       <div className={style.black_navbar}>
+        <img className={style.logo} src={logo}/>
         React
+        <h2 className={style.nav_center}> 문서 자습서 블로그 커뮤니티 </h2>
+        <input className={style.nav_search} type='text' placeholder='검색'/>
+        <h4 className={style.nav_version}>v17.0.2</h4>
       </div> 
-      <div className={style.div1}>??</div>
-      
     </>
   )
 }
