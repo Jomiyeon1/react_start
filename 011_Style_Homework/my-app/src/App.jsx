bcb38fc26c3b6de78b0dfef5e26726065b999987
Mi Yeon <fourp@naver.com>
2024-03-08 15:49:58 +0900

    Quiz Content 초기 시안

--- 011_Style_Homework/my-app/src/App.jsx
+++ 011_Style_Homework/my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import './app.css';
 import Navbar from './Components/Navbar'
+import Contents from './Components/Contents'
 import React, { Fragment } from 'react';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Contents/>
     </>
   );
 }
---
+++ 011_Style_Homework/my-app/src/Components/Contents.jsx
@@ -0,0 +1,12 @@
+import style from './contents.module.css'
+import React, { Fragment } from 'react';
+
+export default function Content() {
+  return (
+    <>
+      <h1>React</h1>
+      <h2>사용자 인터페이스를 만들기 위한 JavaScript 라이브러리</h2>
+      <input type='submit' value='시작하기'/> 자습서 읽어보기
+    </>
+  )
+}
