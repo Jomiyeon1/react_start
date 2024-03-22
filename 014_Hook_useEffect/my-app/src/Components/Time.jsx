002be1fcd481a1e3e74233851a352177a2049373
MY <fourp@naver.com>
2024-03-22 22:37:30 +0900

    Hook_useEffect Quiz

--- 014_Hook_useEffect/my-app/src/Components/Time.jsx
+++ 014_Hook_useEffect/my-app/src/Components/Time.jsx
@@ -5,24 +5,28 @@ export default function Time(props) {
   const [hour, setHour] = useState(today.getHours());
   const [min, setMin] = useState(today.getMinutes());
   const [sec, setSec] = useState(today.getSeconds());
-  console.log("렌더링 :D")//렌더링이 잘 되는지 확인용! 꼭 넣고 진행해주세요.
+  //렌더링 확인용
+  console.log("렌더링 :D")
     // 컴포넌트 렌더링이 끝난 후 useEffect가 실행된다.
     // => update될때 또한 컴포넌트의 리렌더링이 완료된 후 useEffect가 실행
-    // log를 확인하면 렌더링 -> Time End 후 Timer -> 렌더링 -> Time End -> clean up! 반복
-    // 첫 렌더링 후 ( 렌더링 -> Time End ) useEffect(Timer) -> 재렌더링??(렌더링 -> Time End) -> clean up 순
+    // log를 확인하면 렌더링 -> Time End -> Timer//  렌더링 -> Time End -> clean up! -> Timer 반복
     useEffect(() => {
+        console.log("Timer!");
        let currentTime = setInterval(() => {           
-           console.log("Timer");
             setToday(new Date());
             setHour(today.getHours());
             setMin(today.getMinutes());
             setSec(today.getSeconds());
          }, 1000);
          return() => {
-             clearInterval(currentTime);
+            clearInterval(currentTime);
+            //clearInterval을 안하면 시간이 계속 set되므로, 번쩍이며(?) 계속 다시 로딩된다.
+            // 컴포넌트가 실행되기 직전에 반복동작을 삭제하는 부분
             console.log("clean up!");
          }
         }, [today])
+
+        //렌더링 확인용
         console.log("Time End");
 
   return (
