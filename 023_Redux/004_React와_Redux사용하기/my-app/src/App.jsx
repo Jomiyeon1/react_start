e5e8457242d9c21ea1b70d5c9c0db2469ae72e56
Mi Yeon <fourp@naver.com>
2024-04-28 16:52:50 +0900

    리액트에서 리덕스 사용하기(React)

---
+++ 023_Redux/004_React와_Redux사용하기/my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import GoodsCounter from './components/GoodsCounter'
+import StockCounter from './components/StockCounter'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test2</h1>
+      <GoodsCounter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 023_Redux/004_React와_Redux사용하기/my-app/src/components/StockCounter.jsx
@@ -0,0 +1,15 @@
+import { useSelector, useDispatch } from 'react-redux'
+// import { , } from '../modules/stockCounter'
+
+function StockCounter(){
+
+    return (
+        <div>
+            <p>판매중!!</p>
+            <p>완판!!</p>
+            {/* hint : <p>{message}</p> */}
+        </div>
+    )
+}
+
+export default StockCounter
