2a7fa390aaec34a8c65eb154b995599dfd330d41
MiYeon <fourp@naver.com>
2024-09-30 13:35:12 +0900

    블로그 만들기_homepage - banner

--- react-blog/my-app/src/components/banner/Banner.jsx
+++ react-blog/my-app/src/components/banner/Banner.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export default function Banner() {
+  const [blogInfo, setBlogInfo] = useState(undefined); 
+  
+  useEffect(()=>{
+    axios
+     .get('http://localhost:3001/blog')
+     .then((json) => {
+      setBlogInfo(json.data)
+    })
+    .catch((e) => {
+      setBlogInfo(undefined)
+    })
+  },[])
   return (
     <div>
-      Banner
+      {blogInfo === undefined ? <div>로딩중... 😥</div> : (
+        <div className="banner">
+          <div className="max-width">
+            <div className="banner-contents">
+              <p className="sub-text">{blogInfo.subTitle}</p>
+              <p className="main-text">{blogInfo.mainTitle}</p>
+              <p className="description">{blogInfo.description}</p>
+            </div>
+          </div>
+         </div>
+      )}
     </div>
   )
 }
